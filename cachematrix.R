ca4eb7f030031715740ae51df428bd8ef4e246e7
kicior <kiciorek+github@gmail.com>
2015-06-12 11:24:10 +0200

    Really Ready

--- cachematrix.R
+++ cachematrix.R
@@ -2,39 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
-## This function creates a special "matrix" object that can cache its inverse.
+### This function creates a special "matrix" object that can cache its inverse.
+### It can read matrix as argument or cache it later.
 makeCacheMatrix <- function(x = matrix()) {
-        # Setting y to NULL makes possible checking if there is any matrix to invert
-        y <- NULL
+        inv <- NULL
         # This function reads matrix and assigns it to y
-        makeCache <- function(x) y <<- x
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
         # This function reads the value of matrix
-        getCache <- function() y
+        get <- function() x
+        setInv <- function(inverted) inv <<- inverted
+        getInv <- function() inv
         # We are making a list. It will be possible to call it using list subsetting
-        list(makeCache = makeCache, getCache = getCache)
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 ## Write a short comment describing this function
-## This function computes the inverse of the special "matrix" returned by
-## makeCacheMatrix above. If the inverse has already been calculated
-## (and the matrix has not changed), then the cachesolve should retrieve
-## the inverse from the cache.
+### This function computes the inverse of the special "matrix" returned by
+### makeCacheMatrix above. If the inverse has already been calculated
+### (and the matrix has not changed), then the cachesolve should retrieve
+### the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         # Assigning cached (or not) matrix to flag y makes possible to do
         # different things depending of its presence or absence
-        y <- x$getCache()
-        # Giving message if no matrix present
-        if(is.null(y)){
-                message("No matrix to invert")
+        inv <- x$getInv()
+        # Giving message if inverted matrix is cached
+        if(!is.null(inv)){
+                message("Getting cached matrix")
+                return(inv)
         }
-        # Giving message if matrix is present but not square
-        else if(dim(x$getCache())[1]!=dim(x$getCache()[2])){
-                message("Matrix must be square")        
-        }
-        # Inverting matrix and giving message
-        else {
-                message("Inverted matrix is:")
-                mInverse <- solve(x$getCache())
-                mInverse
-        }
+        message("Inverted matrix is:")
+        data <- x$get()
+        mInverse <- solve(data)
+        x$setInv <- mInverse
+        mInverse
 }
